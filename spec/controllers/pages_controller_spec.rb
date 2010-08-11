e6ebf45e84eb66aa9222283eaa3a19bc4d930f68
Clayton Rocker <claysrg@gmail.com>
2010-08-11 14:04:36 -0400

    Added user following

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,18 +7,6 @@ describe PagesController do
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-
-    it "should have the right title" do
-      get 'home'
-      response.should have_tag("title", @base_title + " | Home")
-    end
-  end
-
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
@@ -52,6 +40,41 @@ describe PagesController do
     it "should have the right title" do
       get 'help'
       response.should have_tag("title", @base_title + " | Help")
+    end
+  end
+
+  describe "GET 'home'" do
+
+    describe "when not signed in" do
+
+      before(:each) do
+        get :home
+      end
+
+      it "should be successful" do
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        response.should have_tag("title", "#{@base_title} | Home")
+      end
+    end
+
+    describe "when signed in" do
+
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
+      end
+
+      it "should have the right follower/following counts" do
+        get :home
+        response.should have_tag("a[href=?]", following_user_path(@user),
+                                              /0 following/)
+        response.should have_tag("a[href=?]", followers_user_path(@user),
+                                              /1 follower/)
+      end
     end
   end
 end
